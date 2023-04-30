527a069154fcc5881d01a8dc180b01a41c5f53ac
RomaChernyak <118313101+RomaChernyak@users.noreply.github.com>
2023-04-30 22:56:16 +0300

    tasks 2-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+const pauseTimeByDefault = 0;
+
+console.log(iframe);
+console.log(player);
+
+const onPlay = data => {
+    // console.log(data);
+    const { seconds } = data;
+    localStorage.setItem(STORAGE_KEY, seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const pausePlayer = localStorage.getItem(STORAGE_KEY);
+
+player.setCurrentTime(pausePlayer || pauseTimeByDefault);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,65 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const { email, message } = form;
+const STORAGE_KEY = 'feedback-form-state';
+
+// console.log(email);
+// console.log(message);
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onTextareaInput, 500));
+
+const checkLocalStorage = () => {
+    const storagedData = JSON.parse(
+        localStorage.getItem(STORAGE_KEY)
+    );
+
+    if (localStorage.getItem(STORAGE_KEY)) {
+        email.value = storagedData.email;
+        message.value = storagedData.message;
+    }
+};
+
+checkLocalStorage();
+
+function onTextareaInput(evt) {
+    // console.log(evt.target);
+    // console.log(evt.target.name);
+    // console.log(evt.target.value);
+
+    const {
+        target: { name, value },
+    } = evt;
+
+    if (name === 'email') {
+        localStorage.setItem(
+            STORAGE_KEY,
+            JSON.stringify(
+                { email: value, message: message.value }
+            )
+        )
+    };
+
+    if (name === 'message') {
+        localStorage.setItem(
+            STORAGE_KEY,
+            JSON.stringify(
+                { email: email.value, message: value }
+            )
+        )
+    };
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    evt.target.reset();
+
+    console.log(
+        JSON.parse(localStorage.getItem(STORAGE_KEY))
+    );
+
+    localStorage.removeItem(STORAGE_KEY);
+}
 
